ecd3cebb2b667129021254b121610389a944741a
little-junior <rampagedolol@outlook.com>
2023-07-28 19:12:29 -0300

    add new functions

--- sistema-bancario-dio.py
+++ sistema-bancario-dio.py
@@ -13,7 +13,7 @@ def main():
             print("DIGITE APENAS NUMEROS. TENTE NOVAMENTE")
             return 0
         
-        extrato.append("+" + str(valor_deposito))
+        extrato.append("D: +" + str(valor_deposito))
         return valor_deposito
 
     def sacar(valor_saque):
@@ -31,7 +31,7 @@ def main():
                 print("VOCE TENTOU SACAR MAIS DO QUE O SALDO DISPONIVEL NA CONTA. TENTE NOVAMENTE")
                 return 0
             
-            extrato.append("-" + str(valor_saque))
+            extrato.append("S: -" + str(valor_saque))
         except ValueError:
             print("DIGITE APENAS NUMEROS. TENTE NOVAMENTE")
             return 0
@@ -48,8 +48,21 @@ def main():
         print(f"VALOR NA CONTA: R$ {saldo:.2f}")
         print()
 
+    def cadastrar_usuario(nome, data_nascimento, cpf, endereco):
+        usuarios.append({
+            "usuario" : (len(usuarios) + 1),
+            "nome" : nome,
+            "data_de_nascimento": data_nascimento,
+            "cpf": cpf,
+            "endereco": endereco})
 
-
+    def criar_conta_corrente(cpf_usuario):
+        contas_corrente.append({
+            "cpf_usuario" : cpf_usuario,
+            "num_conta" : (len(contas_corrente) + 1),
+            "agencia" : "0001"
+        })
+        ...
     LIMITE_SAQUES_DIARIOS = 3
     LIMITE_VALOR_SAQUE = 500
     saldo = 0
@@ -57,14 +70,15 @@ def main():
     saques = []
     depositos = []
     extrato = []
-
+    usuarios = []
+    contas_corrente = []
 
     print(f'{" BEM-VINDO AO BANCO DIO! ".center(100, "#")}')
 
 
     while True:
         print()
-        print('DIGITE UMA DAS SEGUINTES OPÇÕES:\n[1] DEPOSITAR\n[2] SACAR\n[3] VER EXTRATO\n[0] SAIR\n')
+        print('DIGITE UMA DAS SEGUINTES OPÇÕES:\n[1] DEPOSITAR\n[2] SACAR\n[3] VER EXTRATO\n[4] CADASTRAR USUARIO\n[5] CRIAR CONTA CORRENTE\n[0] SAIR\n')
         
         opcao_escolhida = input("| ")
         print()
@@ -83,7 +97,7 @@ def main():
             print("VOCE ESCOLHEU A OPÇÃO: SAQUE".center(50, "="))
             quantia_a_sacar = input("DIGITE A QUANTIA QUE DESEJA SACAR: R$ ")
             
-            saque_final = sacar(quantia_a_sacar)
+            saque_final = sacar(valor_saque=quantia_a_sacar)
             total_saques_realizados +=1 if saque_final != 0 else 0
             saldo -= saque_final
             
@@ -91,6 +105,43 @@ def main():
 
         elif opcao_escolhida == "3":
             mostrar_extrato(extrato)
+
+        elif opcao_escolhida == "4":
+            print("VOCE ESCOLHEU A OPÇÃO: CADASTRAR USUÁRIO")
+
+            nome = input("DIGITE O NOME DO USUARIO: ").strip()
+            data_nascimento = input("DIGITE A DATA DE NASCIMENTO: ").strip()
+            cpf = input("DIGITE O CPF: ").replace("-", "").replace(".", "").strip()
+            endereco = input("DIGITE O ENDERECO (logradouro - num - bairro - cidade/estado sigla): ").strip()
+
+            cpf_cadastrado = False
+            for usuario in usuarios:
+                if cpf == usuario["cpf"]:
+                    cpf_cadastrado = True
+                    break
+
+            if cpf_cadastrado:
+                print("USUARIO JÁ CADASTRADO. TENTE NOVAMENTE")
+                continue
+            
+            cadastrar_usuario(nome, data_nascimento, cpf, endereco)
+        
+        elif opcao_escolhida == "5":
+            print("VOCE ESCOLHEU A OPÇÃO: CRIAR CONTA CORRENTE")
+            print()
+
+            cpf_usuario = input("DIGITE O CPF DO USUARIO DA CONTA: ").replace("-", "").replace(".", "").strip()
+
+            
+            for usuario in usuarios:
+                if cpf_usuario == usuario["cpf"]:
+                    criar_conta_corrente(cpf_usuario)
+                    break
+                else:
+                    print("USUARIO NAO ENCONTRADO. TENTE NOVAMENTE")
+            
+            
+            
 
         elif opcao_escolhida == "0":
             print('SAINDO...')
